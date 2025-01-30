65ca853073dde2f6ef87b56e4e3302fb01e373e4
MC_Gaze_at_XM <2471334014@qq.com>
2025-01-30 12:03:45 +0800

    Update EpicFight_oldsolar.java

--- src/main/java/net/EpicFight_oldsolar/EpicFight_oldsolar.java
+++ src/main/java/net/EpicFight_oldsolar/EpicFight_oldsolar.java
@@ -1,1 +1,20 @@
+package net.EpicFight_oldsolar;
 
+import net.EpicFight_oldsolar.Network.PacketHandler;
+import net.EpicFight_oldsolar.capabilites.oldsolarCapability;
+import net.EpicFight_oldsolar.gameasset.EpicFigeht_oldarAnimations;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("epicfight_oldsolar")
+public class EpicFight_oldsolar {
+    public static final String MODID = "epicfight_oldsolar";
+
+    public EpicFight_oldsolar() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(oldsolarCapability::register);
+        bus.addListener(EpicFigeht_oldarAnimations::registerAnimations);
+        PacketHandler.register();
+    }
+}
